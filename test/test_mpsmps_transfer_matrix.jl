e9c4178a881382d42c11f6c86a63061c41268c15
Wei Tang <tangwei@smail.nju.edu.cn>
2024-05-07 17:08:59 +0200

    improve tests for mps-mps transfer matrix

--- test/test_mpsmps_transfer_matrix.jl
+++ test/test_mpsmps_transfer_matrix.jl
@@ -62,26 +62,33 @@ end
 
 
 @testset "test backward for both left_env and right_env" for ix in 1:10
-    sp1 = ℂ^12;
+    spa = ℂ^8;
+    spb = ℂ^12;
     sp2 = ℂ^3;
-    A = TensorMap(rand, ComplexF64, sp1*sp2, sp1);
-    B = TensorMap(rand, ComplexF64, sp1*sp2, sp1);
+    A = TensorMap(rand, ComplexF64, spa*sp2, spa);
+    B = TensorMap(rand, ComplexF64, spb*sp2, spb);
 
     function _F1(X)
         TM1 = MPSMPSTransferMatrix(X, X, false)
-        TM2 = MPSMPSTransferMatrix(X, B, false)
         vl1 = left_env(TM1)
+        vr1 = right_env(TM1)
+        
+        TM2 = MPSMPSTransferMatrix(X, B, false)
+        vl2 = left_env(TM2)
         vr2 = right_env(TM2)
    
-        return norm(tr(vl1)) / norm(vl1) + norm(tr(vr2))/norm(vr2) + norm(tr(vl1 * vr2)) / norm(vl1) / norm(vr2)
+        return norm(tr(vl1 * vr1)) / norm(vl1) / norm(vr1) + norm(tr(vl2 * vr2)) / norm(vl2) / norm(vr2) 
     end
     function _F2(X)
         TM1 = MPSMPSTransferMatrix(A, X, false)
-        TM2 = MPSMPSTransferMatrix(X, X, false)
         vl1 = left_env(TM1)
+        vr1 = right_env(TM1)
+        
+        TM2 = MPSMPSTransferMatrix(X, X, false)
+        vl2 = left_env(TM2)
         vr2 = right_env(TM2)
    
-        return norm(tr(vl1)) / norm(vl1) + norm(tr(vr2))/norm(vr2) + norm(tr(vl1 * vr2)) / norm(vl1) / norm(vr2)
+        return norm(tr(vl1 * vr1)) / norm(vl1) / norm(vr1) + norm(tr(vl2 * vr2)) / norm(vl2) / norm(vr2)
     end
 
     test_ADgrad(_F1, A)
