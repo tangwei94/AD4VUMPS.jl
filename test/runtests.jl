aab26c5351eb6dd64044a62343f8d0c44bcc5f2c
Wei Tang <tangwei@smail.nju.edu.cn>
2024-05-13 11:10:24 +0200

    test ad for vumps_update; passed

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using MPSTransferMatrix
 function test_ADgrad(_F, X)
 
     # retraction direction
-    for i in 1:20
+    for i in 1:10
         sX = similar(X)
         randomize!(sX)
 
@@ -27,6 +27,19 @@ function test_ADgrad(_F, X)
         end
         #@test abs(∂α2 - ∂αad) < 1e-6
     end
+end
+
+# a function that generates MPO, used in tests
+function tensor_square_ising(β::Real)
+    t = TensorMap(ComplexF64[exp(β) exp(-β); exp(-β) exp(β)], ℂ^2, ℂ^2)
+    sqrt_t = sqrt(t)
+    δ = TensorMap(zeros, ComplexF64, ℂ^2*ℂ^2, ℂ^2*ℂ^2)
+
+    δ[1, 1, 1, 1] = 1
+    δ[2, 2, 2, 2] = 1 
+
+    @tensor T[-1 -2 ; -3 -4] := sqrt_t[-1; 1] * sqrt_t[-2; 2] * sqrt_t[3; -3] * sqrt_t[4; -4] * δ[1 2; 3 4]
+    return T
 end
 
 include("test_mpsmps_transfer_matrix.jl");
--- test/test_vumps.jl
+++ test/test_vumps.jl
@@ -29,42 +29,23 @@ end
     QAC = TensorMap(rand, ComplexF64, sp1*sp2, sp1);
     QC = TensorMap(rand, ComplexF64, sp1, sp1);
 
-    function tensor_square_ising(β::Real)
-        t = TensorMap(ComplexF64[exp(β) exp(-β); exp(-β) exp(β)], ℂ^2, ℂ^2)
-        sqrt_t = sqrt(t)
-        δ = TensorMap(zeros, ComplexF64, ℂ^2*ℂ^2, ℂ^2*ℂ^2)
-
-        δ[1, 1, 1, 1] = 1
-        δ[2, 2, 2, 2] = 1 
-
-        @tensor T[-1 -2 ; -3 -4] := sqrt_t[-1; 1] * sqrt_t[-2; 2] * sqrt_t[3; -3] * sqrt_t[4; -4] * δ[1 2; 3 4]
-        return T
-    end
     βc = asinh(1) / 2
     T = tensor_square_ising(βc)
-        
-    function _F1(AL)
-        TM_L = MPSMPOMPSTransferMatrix(AL, T, AL, false)
-        TM_R = MPSMPOMPSTransferMatrix(AR, T, AR, false)
-        EL = left_env(TM_L)
-        ER = right_env(TM_R)
-        C_map = MPSMPSTransferMatrix(EL', ER, false)
-        C = left_env(C_map) 
-        #return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
-        return norm(tr(QC*C)) / norm(C)
+    
+    function _F1(AL1)
+        AC, C = vumps_update(AL1, AR, T)
+        return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
     end
     function _F2(AR1)
         AC, C = vumps_update(AL, AR1, T)
-        #return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
-        return norm(tr(QC*C)) / norm(C)
+        return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
     end
     function _F3(T1)
         AC, C = vumps_update(AL, AR, T1)
-        #return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
-        return norm(tr(QC*C)) / norm(C)
+        return norm(tr(QAC' * AC)) / norm(AC) #+ norm(tr(C)) / norm(C)
     end
   
     test_ADgrad(_F1, AL)
-    #test_ADgrad(_F2, AR)
-    #test_ADgrad(_F3, T)
+    test_ADgrad(_F2, AR)
+    test_ADgrad(_F3, T)
 end
