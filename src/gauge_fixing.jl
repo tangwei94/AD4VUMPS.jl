3d7f3ed6195c30e6c5dcc583aad5def16dd3e055
Wei Tang <wei.tang.nju@gmail.com>
2024-10-15 14:20:17 +0200

    differentiate through the gauge fixing process

--- src/gauge_fixing.jl
+++ src/gauge_fixing.jl
@@ -4,18 +4,43 @@ function gauge_fixing(AL1, AL2)
     U, _ = leftorth(σ; alg=QRpos())
     return U
 end
-@non_differentiable gauge_fixing(args...)
+#@non_differentiable gauge_fixing(args...)
 
 function overall_u1_phase(T1::AbstractTensorMap, T2::AbstractTensorMap)
-    for (f1, f2) in fusiontrees(T1)
-        for ix in 1:length(T1[f1, f2])
-            if norm(T1[f1, f2][ix]) > 1e-2
-                return angle(T1[f1, f2][ix] / T2[f1, f2][ix])
+    for (f1, f2) in fusiontrees(T2)
+        for ix in 1:length(T2[f1, f2])
+            if norm(T2[f1, f2][ix]) > 1e-2
+                return T1[f1, f2][ix] / T2[f1, f2][ix]
             end
         end
     end
 end
-@non_differentiable overall_u1_phase(args...)
+function ChainRulesCore.rrule(::typeof(overall_u1_phase), T1::AbstractTensorMap, T2::AbstractTensorMap)
+    info = []
+    for (f1, f2) in fusiontrees(T2)
+        for ix in 1:length(T2[f1, f2])
+            if norm(T2[f1, f2][ix]) > 1e-2
+                fusiontrees = (f1, f2)
+                index = ix
+                α = T1[f1, f2][ix] / T2[f1, f2][ix]
+                push!(info, (fusiontrees, index, α))
+                break
+            end
+        end
+    end
+    fusiontrees, index, α = info[1] 
+
+    function overall_u1_phase_pushback(∂α)
+        ∂T1 = zero(T1)
+        ∂T2 = zero(T2)
+        ∂T1[fusiontrees][index] = ∂α / T2[fusiontrees][index]
+        ∂T2[fusiontrees][index] = -∂α * T1[fusiontrees][index] / T2[fusiontrees][index]^2
+        return NoTangent(), ∂T1, ∂T2
+    end
+
+    return α, overall_u1_phase_pushback
+end
+#@non_differentiable overall_u1_phase(args...)
 
 #function diagonalize_C(AL::MPSTensor, AR::MPSTensor, AC::MPSTensor, C::MPSTensor)
 #    U, C1, V = tsvd(C)
@@ -34,8 +59,8 @@ function gauge_fixed_vumps_iteration(AL::MPSTensor, AR::MPSTensor, T::MPOTensor)
     @tensor AL1_gauged[-1 -2; -3] := AL1[1 -2; 2] * U[-1; 1] * U'[2; -3]
 
     λ = overall_u1_phase(AL, AL1_gauged)
-    AL1_gauged = AL1_gauged * exp(im*λ)
-    AR1_gauged = AR1_gauged * exp(im*λ)
+    AL1_gauged = AL1_gauged * λ
+    AR1_gauged = AR1_gauged * λ
 
     return AL1_gauged, AR1_gauged 
 end
